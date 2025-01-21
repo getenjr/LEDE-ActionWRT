1405c1dd00bdabb7191d01a13feedddc90207268
Iex <sunsan05@Gmail.com>
2025-01-21 15:03:58 +0800

    TEST

--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Add a feed source
+# Add PassWall feeds
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+# Add SSRP feed source
 sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
