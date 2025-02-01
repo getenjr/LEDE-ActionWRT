cc3b00ecfa2a6652298921eb05bfdb012b344c6b
lex <sunsan05@gmail.com>
2025-02-01 10:24:56 +0800

    提交

--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # Add PassWall feeds
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # Add SSRP feed source
 sed -i "/helloworld/d" "feeds.conf.default"
