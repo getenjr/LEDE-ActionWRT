3e9c39635468e2a90ceb68d9fff9cff8354d1dc4
42 <sunsan05@Gmail.com>
2025-02-04 09:38:55 +0800

    Update r5s-part1.sh

--- SCRIPTS/R5S/r5s-part1.sh
+++ SCRIPTS/R5S/r5s-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Add PassWall feeds
-# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # Add SSRP feed source
-sed -i "/helloworld/d" "feeds.conf.default"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+# sed -i "/helloworld/d" "feeds.conf.default"
+# echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
