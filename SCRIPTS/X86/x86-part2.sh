303e20d0d6f5b4717b9229ed55ddddd05becbc7e
getenjr <32555548+getenjr@users.noreply.github.com>
2025-02-05 18:45:24 +0700

    Update x86-part2.sh

--- SCRIPTS/X86/x86-part2.sh
+++ SCRIPTS/X86/x86-part2.sh
@@ -13,16 +13,16 @@
 # Add applications
 
 # # Passwall
-# git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall_package
-# git clone -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall
-# cp -rf ./package/lean/passwall_package/* ./package/lean/passwall
-# rm -rf ./package/lean/passwall_package
+git clone -b packages --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall_package
+git clone -b luci --depth=1 https://github.com/xiaorouji/openwrt-passwall.git ./package/lean/passwall
+cp -rf ./package/lean/passwall_package/* ./package/lean/passwall
+rm -rf ./package/lean/passwall_package
 
 # # Openclash
-# git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
-# mkdir -p ./package/lean/openclash
-# cp -rf ./package/lean/luci-app-openclash/luci-app-openclash/* ./package/lean/openclash
-# rm -rf ./package/lean/luci-app-openclash
+git clone -b master --depth=1 https://github.com/vernesong/OpenClash.git ./package/lean/luci-app-openclash
+mkdir -p ./package/lean/openclash
+cp -rf ./package/lean/luci-app-openclash/luci-app-openclash/* ./package/lean/openclash
+rm -rf ./package/lean/luci-app-openclash
 
 # Mosdns
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
@@ -39,7 +39,7 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # Modify default Hostname
-sed -i 's/OpenWrt/LEDE/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/GT_WRT/g' package/base-files/files/bin/config_generate
 
 # Clean build directory before compilation
 rm -rf tmp/
