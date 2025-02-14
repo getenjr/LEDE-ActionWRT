906be2bff09a5a7eff84a7621262619f8c6f1a45
getenjr <32555548+getenjr@users.noreply.github.com>
2025-02-15 02:59:04 +0700

    Update x86-part1.sh

--- SCRIPTS/X86/x86-part1.sh
+++ SCRIPTS/X86/x86-part1.sh
@@ -17,3 +17,5 @@
 # Add SSRP feed source
 # sed -i "/helloworld/d" "feeds.conf.default"
 # echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' "feeds.conf.default"
+sed -i '2i src-git small https://github.com/kenzok8/small' "feeds.conf.default"
